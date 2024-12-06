2cb636d6fc176ad0c0ec801f113bafdea199975c
Tuan Khoa Tran Ngo <23130024@student.hcmus.edu.vn>
2024-12-06 20:40:45 +0700

    Đặt lại tên các mảng từ c t x h d thành cong tru nhan chia

--- tinh_da_thuc.c
+++ tinh_da_thuc.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 #include <stdlib.h>
-int a[20],b[20],c[20],t[20],x[40],h[20],d[20],m,n,max;
+int a[20],b[20],tong[20],hieu[20],tich[40],thuong[20],sodu[20],m,n,max;
 int len(int mang[]);
-void nhap(int mang[], int l, char ten);
-void xuat(int mang[], int l, char ten);
-void cong(int a[], int b[]);
-void tru(int a[], int b[]);
-void nhan(int a[], int b[]);
-void chia(int a[], int b[]);
+void nhapmang(int mang[], int l,char ten);
+void xuatmang(int mang[], int l,char ten);
+void congmang(int a[], int b[]);
+void trumang(int a[], int b[]);
+void nhanmang(int a[], int b[]);
+void chiamang(int a[], int b[]);
+
 void main(){
     printf("Gia su ta da rut gon xong bieu thuc, nhap cac he so vao a[] va b[]\nvoi bac thap dung truoc, bac cao dung sau.\n");
     printf("VD 2x^2-2x+1 va x^3-2, thi a[] = 1 -2 2 va b[] = -2 0 0 3\n");
@@ -16,14 +17,19 @@ void main(){
     scanf("%d %d",&m,&n);
     if(m>=n) max=m;
     else max=n;
-    nhap(a,m,'a');
-    nhap(b,n,'b');
-    xuat(a,m,'a');
-    xuat(b,n,'b');
-    cong(a,b);
-    tru(a,b);
-    nhan(a,b);
-    chia(a,b);
+    nhapmang(a,m,'a');
+    nhapmang(b,n,'b');
+    xuatmang(a,m,'a');
+    xuatmang(b,n,'b');
+    congmang(a,b);
+    trumang(a,b);
+    nhanmang(a,b);
+    chiamang(a,b);
+    printf("\ntong da thuc: "); for(int i=0;i<=len(tong);i++) printf("%d ",tong[i]);
+    printf("\nhieu da thuc: "); for(int i=0;i<=len(hieu);i++) printf("%d ",hieu[i]);
+    printf("\ntich da thuc: "); for(int i=0;i<=len(tich);i++) printf("%d ",tich[i]);
+    printf("\nthuong da thuc: "); for(int i=0;i<=len(thuong);i++) printf("%d ",thuong[i]);
+    printf("\nso du da thuc: "); for(int i=0;i<=len(sodu);i++) printf("%d ",sodu[i]);
 }
 
 int len(int mang[20]){
@@ -34,57 +40,52 @@ int len(int mang[20]){
     return length;
 }
 
-void nhap(int mang[],int l,char ten){
+void nhapmang(int mang[],int l,char ten){
     for(int i=0;i<=l;i++){
         printf("%c[%d]= ",ten,i);
         scanf("%d",&mang[i]);
     }
 }
 
-void xuat(int mang[],int l,char ten){
+void xuatmang(int mang[],int l,char ten){
     printf("\nmang %c: ",ten);
     for(int i=0;i<=l;i++)
         printf("%d ",mang[i]);
 }
 
-void cong(int a[], int b[]){
+void congmang(int a[], int b[]){
     for(int i=0;i<=max;i++){
-        c[i]=a[i]+b[i];
+        tong[i]=a[i]+b[i];
     }
-    xuat(c,max,'c');
 }
 
-void tru(int a[], int b[]){
+void trumang(int a[], int b[]){
     for(int i=0;i<=max;i++){
-        t[i]=a[i]-b[i];
+        hieu[i]=a[i]-b[i];
     }
-    xuat(t,max,'t');
 }
 
-void nhan(int a[], int b[]){
+void nhanmang(int a[], int b[]){
     for(int i=0;i<=m;i++){
         for(int j=0;j<=n;j++){
-            x[i+j]+=a[i]*b[j];
+            tich[i+j]+=a[i]*b[j];
         }
     }
-    xuat(x,m+n,'x');
 }
 
-void chia(int a[], int b[]){
+void chiamang(int a[], int b[]){
     int temp[20];
-    for(int i=0;i<=m;i++) d[i]=a[i];
-    while(len(d)>=n){
-        h[len(d)-n]=d[len(d)]/b[n];
+    for(int i=0;i<=m;i++) sodu[i]=a[i];
+    while(len(sodu)>=n){
+        thuong[len(sodu)-n]=sodu[len(sodu)]/b[n];
         for(int i=0;i<=20;i++) temp[i]=0;
-        for(int i=0;i<=len(h);i++){
+        for(int i=0;i<=len(thuong);i++){
             for(int j=0;j<=n;j++){
-                temp[i+j]+=h[i]*b[j];
+                temp[i+j]+=thuong[i]*b[j];
             }
         }
         for(int i=0;i<=m;i++){
-        d[i]=a[i]-temp[i];
+        sodu[i]=a[i]-temp[i];
         }
     }
-    xuat(h,len(h),'h');
-    xuat(d,len(d),'d');
 }
