d6633376f063f6b31051e67bb46552ff2857027c
Tuan Khoa Tran Ngo <23130024@student.hcmus.edu.vn>
2024-12-07 07:19:49 +0700

    sửa các tên biến, tên hàm cho khác với của chương trình đầu

--- tinh_da_thuc.c
+++ tinh_da_thuc.c
@@ -2,8 +2,8 @@
 #include<math.h>
 #include <stdlib.h>
 int a[20],b[20],tong[20],hieu[20],tich[40],thuong[20],sodu[20],so_mu_a,so_mu_b,max_a_b;
-int len(int mang[]);
-void nhapmang(int mang[], int l,char ten);
+int leng(int mang[]);
+void nhapdtmang(int mang[], int l,char ten);
 void xuatmang(int mang[], int l,char ten);
 void congmang(int a[], int b[]);
 void trumang(int a[], int b[]);
@@ -17,22 +17,22 @@ void main(){
     scanf("%d %d",&so_mu_a,&so_mu_b);
     if(so_mu_a>=so_mu_b) max_a_b=so_mu_a;
     else max_a_b=so_mu_b;
-    nhapmang(a,so_mu_a,'a');
-    nhapmang(b,so_mu_b,'b');
+    nhapdtmang(a,so_mu_a,'a');
+    nhapdtmang(b,so_mu_b,'b');
     xuatmang(a,so_mu_a,'a');
     xuatmang(b,so_mu_b,'b');
     congmang(a,b);
     trumang(a,b);
     nhanmang(a,b);
     chiamang(a,b);
-    printf("\ntong da thuc: "); for(int i=0;i<=len(tong);i++) printf("%d ",tong[i]);
-    printf("\nhieu da thuc: "); for(int i=0;i<=len(hieu);i++) printf("%d ",hieu[i]);
-    printf("\ntich da thuc: "); for(int i=0;i<=len(tich);i++) printf("%d ",tich[i]);
-    printf("\nthuong da thuc: "); for(int i=0;i<=len(thuong);i++) printf("%d ",thuong[i]);
-    printf("\nso du da thuc: "); for(int i=0;i<=len(sodu);i++) printf("%d ",sodu[i]);
+    printf("\ntong da thuc: "); for(int i=0;i<=leng(tong);i++) printf("%d ",tong[i]);
+    printf("\nhieu da thuc: "); for(int i=0;i<=leng(hieu);i++) printf("%d ",hieu[i]);
+    printf("\ntich da thuc: "); for(int i=0;i<=leng(tich);i++) printf("%d ",tich[i]);
+    printf("\nthuong da thuc: "); for(int i=0;i<=leng(thuong);i++) printf("%d ",thuong[i]);
+    printf("\nso du da thuc: "); for(int i=0;i<=leng(sodu);i++) printf("%d ",sodu[i]);
 }
 
-int len(int mang[20]){
+int leng(int mang[20]){
     int length=0;
     for(int i=0;i<=19;i++){
         if(mang[i]!=0) length=i;
@@ -40,7 +40,7 @@ int len(int mang[20]){
     return length;
 }
 
-void nhapmang(int mang[],int l,char ten){
+void nhapdtmang(int mang[],int l,char ten){
     for(int i=0;i<=l;i++){
         printf("%c[%d]= ",ten,i);
         scanf("%d",&mang[i]);
@@ -76,10 +76,10 @@ void nhanmang(int a[], int b[]){
 void chiamang(int a[], int b[]){
     int temp[20];
     for(int i=0;i<=so_mu_a;i++) sodu[i]=a[i];
-    while(len(sodu)>=so_mu_b){
-        thuong[len(sodu)-so_mu_b]=sodu[len(sodu)]/b[so_mu_b];
+    while(leng(sodu)>=so_mu_b){
+        thuong[leng(sodu)-so_mu_b]=sodu[leng(sodu)]/b[so_mu_b];
         for(int i=0;i<=20;i++) temp[i]=0;
-        for(int i=0;i<=len(thuong);i++){
+        for(int i=0;i<=leng(thuong);i++){
             for(int j=0;j<=so_mu_b;j++){
                 temp[i+j]+=thuong[i]*b[j];
             }
